a3aa55cd71383d3442bf540b6abb6d48d0d40abc
Arthur Galdino <arthurgaldinoo@hotmail.com>
2020-02-04 10:39:05 -0300

    Uso dos operadores logicos e ifs para calcular IMC

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,31 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutri";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+
+var pesoEhValido = true;
+var alturaEhValido = true;
+
+if(peso <= 0 || peso >= 500){
+    pesoEhValido = false;
+}
+
+if(altura <= 0 || altura >= 3.00){
+    alturaEhValido = false;
+}
+
+if(pesoEhValido && alturaEhValido){
+    var imc = peso / (altura*altura);
+    tdImc.textContent = imc;
+}else{
+    tdImc.textContent = "Altura e/ou Peso invalido";
+}
