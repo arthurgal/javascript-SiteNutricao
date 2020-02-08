20c9fd359046e38affdda879335886ee2cabbdf3
Arthur Galdino <arthurgaldinoo@hotmail.com>
2020-02-08 13:43:38 -0300

    Usei JS para calcular todos os IMC e usei o JS para pegar algo do CSS para alterar a cor de uma linha

--- js/principal.js
+++ js/principal.js
@@ -1,31 +1,35 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutri";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+for(var i = 0; i < pacientes.length; i++){
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+    var tdPeso = pacientes[i].querySelector(".info-peso");
+    var tdAltura = pacientes[i].querySelector(".info-altura");
 
-var tdImc = paciente.querySelector(".info-imc");
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var tdImc = pacientes[i].querySelector(".info-imc");
 
 
-var pesoEhValido = true;
-var alturaEhValido = true;
+    var pesoEhValido = true;
+    var alturaEhValido = true;
 
-if(peso <= 0 || peso >= 500){
-    pesoEhValido = false;
+    if(peso <= 0 || peso >= 500){
+        pesoEhValido = false;
+    }
+
+    if(altura <= 0 || altura >= 3.00){
+        alturaEhValido = false;
+    }
+
+    if(pesoEhValido && alturaEhValido){
+        var imc = peso / (altura*altura);
+        tdImc.textContent = imc.toFixed(2);
+    }else{
+        tdImc.textContent = "Altura e/ou Peso invalido";
+        pacientes[i].classList.add("paciente-invalido");
+    }
+
 }
 
-if(altura <= 0 || altura >= 3.00){
-    alturaEhValido = false;
-}
-
-if(pesoEhValido && alturaEhValido){
-    var imc = peso / (altura*altura);
-    tdImc.textContent = imc;
-}else{
-    tdImc.textContent = "Altura e/ou Peso invalido";
-}
